3d95f6a3ffed3658b080723636de3114e68ade92
TauMah <lenitramc@gmail.com>
2024-12-29 13:55:15 +0100

    Suppression de la carte vue web

--- routes/web.py
+++ routes/web.py
@@ -5,23 +5,23 @@ from flask import render_template, request, session
 from os import path
 import datetime
 
-@app.route("/map", methods=["GET"])
-def map():
-    with open(HERE + "/data/users.json") as f:
-        users = json.load(f)
+# @app.route("/map", methods=["GET"])
+# def map():
+#     with open(HERE + "/data/users.json") as f:
+#         users = json.load(f)
     
-    with open(HERE + "/data/map.json") as f:
-        map = json.load(f)
+#     with open(HERE + "/data/map.json") as f:
+#         map = json.load(f)
 
-    data = {}
-    for k, v in map.items():
-        data[k] = { "color" : "255|255|255", "name" : ""}
-        if v["owner"] != "":
-            data[k]["color"] = users[v["owner"]]["color"]
-            data[k]["owner"] = v["owner"]
-            data[k]["guild"] = users[v["owner"]]["guild"]
+#     data = {}
+#     for k, v in map.items():
+#         data[k] = { "color" : "255|255|255", "name" : ""}
+#         if v["owner"] != "":
+#             data[k]["color"] = users[v["owner"]]["color"]
+#             data[k]["owner"] = v["owner"]
+#             data[k]["guild"] = users[v["owner"]]["guild"]
 
-    return render_template("map.html", data = data)
+#     return render_template("map.html", data = data)
 
 @app.route("/")
 def index():
